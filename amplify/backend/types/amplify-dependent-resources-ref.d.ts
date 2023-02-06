b195d22324e3d7de18d2e5eaca39b9c4e31d89c7
Raki Diallo <raki@Rakis-MBP.mshome.net>
2023-02-06 09:15:59 +0000

    merge

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quesraSample": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
